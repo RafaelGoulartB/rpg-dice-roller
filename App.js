be9bbdcb9e7c801a19ecc3511963f01862b4bd9f
RafaelGoulartB <rafagoulartb@gmail.com>
2020-04-08 09:09:53 -0300

    Adding Assets and Basic Files

--- App.js
+++ App.js
@@ -3,17 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Text>Hello WOrld</Text>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
